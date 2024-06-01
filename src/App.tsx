a32688b8e38e063dc806503a16fbeebfa70bff45
hudson221b <241zxy@gmail.com>
2024-06-01 11:59:54 -0400

    add todos state and add function

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
 import "./App.css"
 
 function App() {
-  const [isAdding, setIsAdding] = useState(false)
+  const [isAdding, setIsAdding] = useState<boolean>(false)
+  const [todos, setTodos] = useState<string[]>([])
+  const [inputValue, setInputValue] = useState<string>("")
+
+  const listItems = useMemo(() => todos.map(todo => <li>{todo}</li>), [todos])
 
   const create = () => {
     setIsAdding(false)
+    const newTodos = [inputValue, ...todos]
+    setTodos(newTodos)
   }
   return (
     <div className="container">
       <h1>My Todo List</h1>
-      <li>sample todo</li>
+      {listItems}
       <div className="input-container" hidden={!isAdding}>
-        <input />
+        <input
+          onChange={e => setInputValue(e.target.value)}
+          value={inputValue}
+        />
         <button onClick={create}>Create</button>
       </div>
       <button onClick={() => setIsAdding(true)} hidden={isAdding}>
