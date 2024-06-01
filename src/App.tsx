1f6996479099c3c8e93db608d57654fce415a6dd
hudson221b <241zxy@gmail.com>
2024-06-01 11:54:04 -0400

    add basic show/hide func to buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { useState } from "react"
 import "./App.css"
 
 function App() {
+  const [isAdding, setIsAdding] = useState(false)
+
+  const create = () => {
+    setIsAdding(false)
+  }
   return (
     <div className="container">
       <h1>My Todo List</h1>
       <li>sample todo</li>
-      <button>Add new</button>
+      <div className="input-container" hidden={!isAdding}>
+        <input />
+        <button onClick={create}>Create</button>
+      </div>
+      <button onClick={() => setIsAdding(true)} hidden={isAdding}>
+        Add New
+      </button>
     </div>
   )
 }
